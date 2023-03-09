08d02c3c97198303d2307e1028b35ea6d2c89ca6
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-03-10 01:09:55 +0200

    Feat: added fn to delete invalid files from s3

--- Backend/src/main/java/com/example/Backend/connectionWithS3/S3DeleteInvalidFiles.java
+++ Backend/src/main/java/com/example/Backend/connectionWithS3/S3DeleteInvalidFiles.java
@@ -3,6 +3,31 @@ package com.example.Backend.connectionWithS3;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
 
 public class S3DeleteInvalidFiles {
+
+    final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+
+
+    public void deleteFile(String bucketName, String[] objectsKeys){
+
+        try
+
+        {
+            DeleteObjectsRequest dor = new DeleteObjectsRequest(bucketName)
+                    .withKeys(objectsKeys);
+            s3.deleteObjects(dor);
+        } catch(
+                AmazonServiceException e)
+
+        {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+
+
+    }
 }
