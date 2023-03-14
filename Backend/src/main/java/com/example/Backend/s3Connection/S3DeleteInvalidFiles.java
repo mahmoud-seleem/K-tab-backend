16e0bd3d6fb61869a30ea9c640e0094d1f23b713
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-03-15 00:10:37 +0200

    fix: fixing issues in deleteinvalid class

--- Backend/src/main/java/com/example/Backend/s3Connection/S3DeleteInvalidFiles.java
+++ Backend/src/main/java/com/example/Backend/s3Connection/S3DeleteInvalidFiles.java
@@ -1,16 +1,9 @@
 package com.example.Backend.s3Connection;
-
-import ch.qos.logback.core.joran.action.AppenderRefAction;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.DeleteObjectsRequest;
 import java.util.List;
-import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +11,18 @@ import org.springframework.stereotype.Component;
 public class S3DeleteInvalidFiles {
     @Autowired
     private S3Utils s3Utils;
+
     public  void deleteInvalidFiles(List<String> keys){
         s3Utils.initializeTheClient();
         String[] keysArray = keys.toArray(new String[keys.size()]);
+        DeleteObjectsRequest request = new DeleteObjectsRequest(s3Utils.getBucketName()).withKeys(keysArray);
+        System.out.println("Deleting the following Amazon S3 objects created by Amazon Pinpoint:");
+        for (String key : keys) {
+            System.out.println("\t- " + key);
+        }
+        try {
+            s3Utils.getAmazonS3().deleteObjects(request);
+        } catch (AmazonServiceException e) {
             System.err.println(e.getErrorMessage());
             System.exit(1);
         }
