c857f30ac87ba9cdcc20220b9f396a04d891441a
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-03-14 15:43:46 +0200

    fix: fixed the dependency issue

--- Backend/src/main/java/com/example/Backend/s3Connection/S3DeleteInvalidFiles.java
+++ Backend/src/main/java/com/example/Backend/s3Connection/S3DeleteInvalidFiles.java
@@ -1,38 +1,11 @@
 package com.example.Backend.s3Connection;
 
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.DeleteObjectsRequest;
-import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.List;
-import java.util.Random;
-import software.amazon.awssdk.core.waiters.WaiterResponse;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.model.CreateMultipartUploadResponse;
-import software.amazon.awssdk.services.s3.model.CompletedMultipartUpload;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.CompletedPart;
-import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
-import software.amazon.awssdk.services.s3.model.UploadPartRequest;
-import software.amazon.awssdk.services.s3.model.CompleteMultipartUploadRequest;
-import software.amazon.awssdk.services.s3.waiters.S3Waiter;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
+
 
 
 public class S3DeleteInvalidFiles {
@@ -40,7 +13,6 @@ public class S3DeleteInvalidFiles {
 
     public static void deleteFilesFromS3(String s3BucketName, List<String> keys){
 
-        Region region = Region.US_EAST_1;
 
         AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
 
