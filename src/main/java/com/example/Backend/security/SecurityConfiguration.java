2475da480f853c6c614e70cf2581cb218499aadb
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2023-05-22 17:38:01 +0300

    fix cors with security

--- src/main/java/com/example/Backend/security/SecurityConfiguration.java
+++ src/main/java/com/example/Backend/security/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests(
                         requests -> requests
